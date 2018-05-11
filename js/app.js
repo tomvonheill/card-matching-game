deb7fe0d9219139cba30b386e168a5cddbf3da31
tomvonheill <tomvonheill@gmail.com>
2018-05-11 09:15:52 -0500

    working deck creation and restart buttons

--- js/app.js
+++ js/app.js
@@ -10,6 +10,122 @@
  *   - add each card's HTML to the page
  */
 
+/*
+*Card object
+*/
+class Card{
+	constructor(cardType){
+		this.cardType = cardType;
+		this.up = false;
+		this.matched = false;
+		var newcard = document.createElement('li');
+  		newcard.className = "card";
+	  	var newi = document.createElement('i');
+	  	newi.classList.add('fa', cardType);
+	  	newcard.appendChild(newi);
+	  	this.element=newcard;
+	}
+}
+
+
+
+
+
+class Deck{
+
+	constructor(){
+		console.log("new deck being made")
+		this.cards = [];
+		this.deckNode = document.getElementById("deck");
+		console.log(this.deckNode)
+		this.cardTypes = ["fa-diamond", "fa-diamond", "fa-anchor","fa-anchor", "fa-bolt",
+	 		"fa-bolt", "fa-cube", "fa-cube", "fa-leaf","fa-leaf", "fa-bicycle","fa-bicycle",
+	  		"fa-bomb", "fa-bomb", "fa-paper-plane-o", "fa-paper-plane-o"];
+	  	for(var i = 0; i<this.cardTypes.length; i++)
+	  		{	
+	  			var Cardi = new Card(this.cardTypes[i]);
+	  			this.deckNode.appendChild(Cardi.element);
+				this.cards.push(Cardi);
+	  		}
+	  	}
+
+	 
+
+	printDeckNode(){
+	  	console.log(this.deckNode)
+  	}
+
+	
+
+
+  	
+  	restart(){
+		console.log("restart my deck");
+		console.log(this.cardTypes);
+		console.log(this.deckNode);
+		while(this.deckNode.firstChild){
+	 		this.deckNode.removeChild(this.deckNode.firstChild);
+		}
+		this.cards = [];
+		this.cardTypes = shuffle(this.cardTypes);
+
+		for(var i = 0; i<this.cardTypes.length; i++){
+			var Cardi = new Card(this.cardTypes[i]);
+	  		this.deckNode.appendChild(Cardi.element);
+			this.cards.push(Cardi);
+
+		}
+	}
+
+	addCard(){
+		this.deckNode.appendChild(Card.element);
+		this.cards.append(Card);
+	}	
+}
+card = new Card("cardtype");
+console.log(card);
+myDeck = new Deck();
+var restartButton = document.getElementById("restart");
+restartButton.addEventListener('click', myDeck.restart.bind(myDeck));
+//myDeck.printDeckNode()
+
+
+
+function startOver(){
+	/*
+	*Remove all children of class deck and then re-populate them
+	*/
+	console.log("start over clicked")
+	var deckNode = document.getElementById("deck")
+	while(deckNode.firstChild){
+		deckNode.removeChild(deckNode.firstChild);
+	}
+
+	var cards = ["fa-diamond", "fa-diamond", "fa-anchor","fa-anchor", "fa-bolt",
+ 		"fa-bolt", "fa-cube", "fa-cube", "fa-leaf","fa-leaf", "fa-bicycle","fa-bicycle",
+  		"fa-bomb", "fa-bomb", "fa-paper-plane-o", "fa-paper-plane-o"]
+  	cards = shuffle(cards);
+
+  	for(var i = 0; i<cards.length; i++)
+  	{
+  		cardType = cards[i];
+  		var newcard = document.createElement('li');
+  		newcard.className = "card";
+  		var newi = document.createElement('i');
+  		newi.classList.add('fa', cardType);
+  		newcard.appendChild(newi);
+  		deckNode.appendChild(newcard);
+
+  	}
+
+
+  	
+
+}
+
+
+ 
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;

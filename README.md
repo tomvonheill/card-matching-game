509f5d4c327e1df61fbc6f14ee174773419c2e83
tomvonheill <tomvonheill@gmail.com>
2018-06-06 11:11:27 -0500

    final version of memory game

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
 # Memory Game Project
 
-## Table of Contents
+## Functionality
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match! Lose a star every 15 moves you make!
 
-## Instructions
+## Interesting project points
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+- Animations are unique
 
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+- Object oriented javascript was used
--- js/app.js
+++ js/app.js
@@ -25,49 +25,81 @@ class Card{
 	  	newcard.appendChild(newi);
 	  	this.element=newcard;
 	}
+
+	flipCard(){
+		if (this.element.classList.contains("open")){
+			this.element.classList.remove("open", "show");
+			this.up = false;
+		}
+
+		else{
+			this.element.classList.add("open", "show");
+			document.getElementById("moves").textContent=Number(document.getElementById("moves").textContent)+1
+			this.up = true;
+		}
+		
+	}
+
+	wrong(){
+		this.up = false;
+
+		if (this.element.classList.contains("open")){
+			this.element.classList.add("wrong");
+			this.element.classList.remove("open", "show");
+			}
+		else{
+			this.element.classList.add("wrong");
+		}
+		
+
+	}
+
+	match(){
+		this.matched = true;
+		this.up = false;
+		this.element.classList.remove("open", "show", "wrong");
+		this.element.classList.add("match");
+	}
 }
-
-
-
 
 
 class Deck{
 
 	constructor(){
-		console.log("new deck being made")
+		console.log("new deck being made");
 		this.cards = [];
 		this.deckNode = document.getElementById("deck");
-		console.log(this.deckNode)
 		this.cardTypes = ["fa-diamond", "fa-diamond", "fa-anchor","fa-anchor", "fa-bolt",
 	 		"fa-bolt", "fa-cube", "fa-cube", "fa-leaf","fa-leaf", "fa-bicycle","fa-bicycle",
 	  		"fa-bomb", "fa-bomb", "fa-paper-plane-o", "fa-paper-plane-o"];
+	  	this.flipped1 = null;
+	  	this.flipped2 = null;
+	  	document.getElementById("moves").textContent=0;
 	  	for(var i = 0; i<this.cardTypes.length; i++)
 	  		{	
 	  			var Cardi = new Card(this.cardTypes[i]);
 	  			this.deckNode.appendChild(Cardi.element);
 				this.cards.push(Cardi);
 	  		}
+
+	  	//timer variables
+	  	//Timer 
+
+
 	  	}
-
-	 
-
-	printDeckNode(){
-	  	console.log(this.deckNode)
-  	}
-
-	
-
-
   	
   	restart(){
+  		clearInterval(interval)
+  		setInterval(interval)
 		console.log("restart my deck");
-		console.log(this.cardTypes);
-		console.log(this.deckNode);
+		modal.style.visibility = 'hidden';
+		restoreStars();
 		while(this.deckNode.firstChild){
 	 		this.deckNode.removeChild(this.deckNode.firstChild);
 		}
 		this.cards = [];
 		this.cardTypes = shuffle(this.cardTypes);
+		document.getElementById("moves").textContent=0;
 
 		for(var i = 0; i<this.cardTypes.length; i++){
 			var Cardi = new Card(this.cardTypes[i]);
@@ -80,51 +112,180 @@ class Deck{
 	addCard(){
 		this.deckNode.appendChild(Card.element);
 		this.cards.append(Card);
-	}	
-}
-card = new Card("cardtype");
-console.log(card);
-myDeck = new Deck();
-var restartButton = document.getElementById("restart");
-restartButton.addEventListener('click', myDeck.restart.bind(myDeck));
-//myDeck.printDeckNode()
-
-
-
-function startOver(){
-	/*
-	*Remove all children of class deck and then re-populate them
-	*/
-	console.log("start over clicked")
-	var deckNode = document.getElementById("deck")
-	while(deckNode.firstChild){
-		deckNode.removeChild(deckNode.firstChild);
 	}
 
-	var cards = ["fa-diamond", "fa-diamond", "fa-anchor","fa-anchor", "fa-bolt",
- 		"fa-bolt", "fa-cube", "fa-cube", "fa-leaf","fa-leaf", "fa-bicycle","fa-bicycle",
-  		"fa-bomb", "fa-bomb", "fa-paper-plane-o", "fa-paper-plane-o"]
-  	cards = shuffle(cards);
+	countFlipped(){
+		var flipped = 0;
+		for (var i=0; i<this.cards.length; i++){
+			flipped = flipped + this.cards[i].up;
+		}
+		return flipped
+	}
 
-  	for(var i = 0; i<cards.length; i++)
-  	{
-  		cardType = cards[i];
-  		var newcard = document.createElement('li');
-  		newcard.className = "card";
-  		var newi = document.createElement('i');
-  		newi.classList.add('fa', cardType);
-  		newcard.appendChild(newi);
-  		deckNode.appendChild(newcard);
+	countMatched(){
+		var matched = 0;
+		for (var i=0; i<this.cards.length; i++){
+			matched = matched + this.cards[i].matched;
+		}
+		console.log(matched)
+		return matched
+	}
 
-  	}
+	removeAllWrongs(){
+		for(var i=0; i<this.deckNode.children.length;i++){
+			if(this.deckNode.children[i].classList.remove("wrong"));
+		}
+	}
+
+	flipCard(clickedCard){
+		var cardNode = clickedCard.target;
+		if(cardNode.classList.contains("match")){
+			return;
+		}
+		var currentlyFlipped = this.countFlipped()
+		this.removeAllWrongs();
 
 
-  	
+		if(currentlyFlipped==1){
+			for (var i=0; i<this.cards.length; i++){
+				if(cardNode==this.cards[i].element & this.cards[i]!==this.flipped1){
+				/*
+				* Flip over second card if there is one flipped card and then
+				*deal with how to handle them
+				*/
+					//this.cards[i].flipCard();
+					this.flipped2 = this.cards[i];
+					if(this.flipped1.element.firstChild.classList[1]== this.flipped2.element.firstChild.classList[1]){
+						document.getElementById("moves").textContent=Number(document.getElementById("moves").textContent)+1
+						this.flipped1.match();
+						this.flipped2.match();
+						if(this.countMatched() == this.cards.length){
+							console.log("somehow equal");
+							console.log(this.cards.length);
+							modal.style.visibility = 'visible';
+							copyToModalStars();
+							stopTimer();
+						}
+					}
+					else{
+						document.getElementById("moves").textContent=Number(document.getElementById("moves").textContent)+1
+						this.flipped1.wrong();
+						this.flipped2.wrong();
+						this.flipped1 = null;
+						this.flipped2 = null;
+
+					}
+
+					conditionallyRemoveStar(Number(document.getElementById("moves").textContent))
+				}
+			}
+		}
+
+		if(currentlyFlipped== 0){
+			for (var i=0; i<this.cards.length; i++){
+				if(cardNode==this.cards[i].element){
+				/*
+				* Flip ove the card if there are 1 or less cards flipped
+				*/
+					this.flipped1 = this.cards[i]
+					this.flipped1.flipCard();
+					conditionallyRemoveStar(Number(document.getElementById("moves").textContent))
+					
+				}	
+			}
+		}
+	}
+}
+
+card = new Card("cardtype");
+myDeck = new Deck();
+
+var seconds = 0; 
+var tens = 0; 
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
+var appendTensPlay = document.getElementById("tensPlay");
+var appendSecondsPlay = document.getElementById("secondsPlay");
+var interval = setInterval(startTimer,10)
+
+var restartButton = document.getElementById("restart");
+restartButton.addEventListener('click', myDeck.restart.bind(myDeck));
+myDeck.deckNode.addEventListener('click', myDeck.flipCard.bind(myDeck));
+restartButton.addEventListener('click', restartTimer)
+
+var playAgainButton = document.getElementById("play-again-button")
+playAgainButton.addEventListener('click', myDeck.restart.bind(myDeck))
+playAgainButton.addEventListener('click',restartTimer)
+
+
+function restoreStars(){
+	var mystars = document.getElementsByClassName("fa fa-star-o")
+	console.log(mystars)
+	while (mystars.length>0) {
+		mystars.item(mystars.length-1).classList.replace('fa-star-o', 'fa-star')
+		console.log("replaced")
+	}
+}
+
+function conditionallyRemoveStar(moves){
+	//Remove a star every 15 moves
+	if(moves%15==0){
+			var panelStars = document.getElementById("panel-stars")
+			var stars = panelStars.getElementsByClassName('fa-star')
+	//Make sure there is at least one star, then remove the last one
+		if (stars.length!=0 && stars!=null) {
+			stars[stars.length-1].classList.replace('fa-star', 'fa-star-o')
+		}
+
+	}
 
 }
 
 
- 
+function startTimer () {
+
+	//timer logic taken from https://codepen.io/cathydutton/pen/GBcvo/ on 5/20/2018
+	tens++; 
+	if(tens < 9){
+	  appendTens.innerHTML = "0" + tens;
+	}
+
+	if (tens > 9){
+	  appendTens.innerHTML = tens;
+	  
+	} 
+
+	if (tens > 99) {
+	  seconds++;
+	  appendSeconds.innerHTML = "0" + seconds;
+	  tens = 0;
+	  appendTens.innerHTML = "0" + 0;
+	}
+
+	if (seconds > 9){
+	  appendSeconds.innerHTML = seconds;
+	}
+}
+
+function restartTimer(){
+	seconds = 0; 
+	tens = 0; 
+	clearInterval(interval);
+	interval = setInterval(startTimer, 10);
+}
+
+function stopTimer(){
+	appendTensPlay.textContent = appendTens.textContent;
+	appendSecondsPlay.textContent = appendSeconds.textContent;
+	clearInterval(interval);
+}
+
+function copyToModalStars(){
+	var panelStars = document.getElementById('panel-stars');
+	var modalStars = document.getElementById('modal-stars');
+	modalStars.innerHTML = panelStars.innerHTML;
+
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -141,14 +302,3 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
